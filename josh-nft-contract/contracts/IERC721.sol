1363c925743d472a8aaedd65599b5f622e837f8d
Bytangle <dev.bytangle@gmail.com>
2022-08-27 10:09:59 +0100

    add ERC165 interface and rename

---
+++ josh-nft-contract/contracts/IERC721.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity ^0.8.0;
+
+import "./IERC165.sol";
+
+/**
+ * @title ERC721 Non-Fungible token standard Interface
+ * @dev Details can be found here: https://github.com/ethereum/EIPs/blob/master/EIPS/eip-721.md
+ */
+interface IERC721 is ERC165 {
+    
+}
