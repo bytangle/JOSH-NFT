b2b3b83c11f41903456bacbe1911abcf81d25adc
Bytangle <dev.bytangle@gmail.com>
2022-08-27 13:45:33 +0100

    Update ERC165 Interface

--- josh-nft-contract/contracts/IERC165.sol
+++ josh-nft-contract/contracts/IERC165.sol
@@ -4,18 +4,13 @@ pragma solidity ^0.8.0;
 
 /**
  * @title ERC165 Interface
- * @dev Note: the ERC-165 identifier for this interface is 0x150b7a02.
  */
 interface IERC165 {
-    /** 
-     * @notice Handle the receipt of an NFT
-     * @dev The ERC721 smart contract calls this function on the recipient
-     * after a `transfer`. Note: the contract address is always the message sender.
-     * @param _operator The address which called `safeTransferFrom` function
-     * @param _from The address which previously owned the token
-     * @param _tokenId The NFT identifier which is being transferred
-     * @param _data Additional data with no specified format
-     * @return `bytes4(keccak256("onERC721Received(address,address,uint256,bytes)"))` unless throwing
-    */
-    function onERC721Received(address _operator, address _from, uint256 _tokenId, bytes memory _data) external returns (bytes4);
+    /**
+     * @notice queries if a contract supports an interface
+     * @dev Interface identification is specified in ERC-165. This function uses less than 30,000 gas.
+     * @param _interfaceID Interface identifier as specified in ERC-165
+     * @return `true` if the contract implements `_interfaceID` and `_interfaceID` is not 0xffffffff, `false` otherwise
+     */
+    function supportsInterface(bytes4 _interfaceID) external view returns (bool);
 }
