574bed30f791a612c9af58856835b0cc8000464b
Bytangle <dev.bytangle@gmail.com>
2022-08-29 21:10:57 +0100

    Add ERC165.sol

---
+++ josh-nft-contract/contracts/ERC165.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity ^0.8.0;
+
+import "./IERC165.sol";
+
+contract ERC165 is IERC165 {
+
+    /// @notice this contract interfaceId
+    bytes4 private constant _INTERFACE_ID_ERC165 = 0x01ffc9a7;
+
+    /// @notice map supported function signatures to bool signifying if they're supported or not
+    mapping(bytes4 => bool) supportedInterfaces_;
+
+    constructor() {
+        _registerInterface(_INTERFACE_ID_ERC165);
+    }
+
+    /**
+     * @notice register supported interfaceId
+     * @dev throws if `_sig` is `0xffffffff`
+     * @param _sig the interfaceId
+     */
+    function _registerInterface(bytes4 _sig) internal {
+        require(_sig != 0xffffffff);
+        supportedInterfaces_[_sig] = true;
+    }
+
+    /**
+     * @notice check if the `_interfaceID` is supported
+     * @param _interfaceID the interfaceId to check
+     * @return returns `true` if supported otherwise `false`
+     */
+    function supportsInterface(bytes4 _interfaceID) public view returns (bool) {
+        return supportedInterfaces_[_interfaceID];
+    }
+}
